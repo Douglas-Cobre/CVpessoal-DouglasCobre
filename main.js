ee98648bf849a2d8064da83e0f830f92338b09bd
Douglas Cobre <doug.cobre@gmail.com>
2024-08-19 18:22:28 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+//Criar uma página com 4 áreas => Sobre mim | Formação | Portfólio | Contato
+//Deve conter LINKS INTERNOS para navegar entre áreas
+//Todo conteúdo pode estar na mesma página
+//Todo o conteúdo também pode ser feito usando 4 páginas
+//Ex de link interno:
+/*
+<ul>
+    <li><a href='#sobre mim'>Sobre mim</a></li> => Quando apertar no link irá para o ID 'sobre mim'
+</ul>
+
+<h1 id='sobre mim'>Sobre mim</h1>
+*/
+
+
+//Sobre mim: falar sobre você, hobyes, gostos, história
+//Formação: faculade, cursos, experiência
+//Portfólio: trabalhos realizados => pode usar links para outras páginas
+//Contato: fazer a parte visuaç do formulário => Utilizar INPUTS (Nome, email, mensagem)
+
+
+//Colocar o trablho em uma hospedagem gratuita => Usar o nome como parte do domínio
+//Usar WEBHOST ou GITHUB PAGES (preferêncial)
+
+let array = ['Douglas', 'Vitor','Heitor','Lara']
+
+console.log(array.length('Lara'))
